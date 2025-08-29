b82fac1005d0cd776b8411c9f5d7cf95bfb8bbb0
MA8052 <mahmoodalam1415@gmail.com>
2025-08-29 11:16:56 +0530

    Update settings.py

--- HomeAutomationProject/settings.py
+++ HomeAutomationProject/settings.py
@@ -39,11 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'HomeLightApp',
-    'tailwind',
-    'TailwindCSS',
-    'django_browser_reload',
 ]
-TAILWIND_APP_NAME ='TailwindCSS'
+
 INTERNAL_IPS = [
 "127.0.0.1",
 
@@ -57,7 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_browser_reload.middleware.BrowserReloadMiddleware',
+  
 ]
 
 ROOT_URLCONF = 'HomeAutomationProject.urls'
